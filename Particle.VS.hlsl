5c76d3dacbd07bc87a17cdec30e9c130155a4989
mutoairi <146906672+mutoairi@users.noreply.github.com>
2024-10-28 14:10:50 +0900

    変更

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -1,4 +1,24 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+#include"Particle.hlsli"
+
+struct TransformationMatrix
 {
-	return pos;
+    float32_t4x4 WVP;
+    float32_t4x4 World;
+};
+StructuredBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
+
+struct VertexShaderInput
+{
+    float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
+    float32_t3 normal : NORMAL0;
+};
+
+VertexShaderOutput main(VertexShaderInput input,uint32_t instanceId:SV_InstanceID)
+{
+    VertexShaderOutput output;
+    output.position = mul(input.position, gTransformationMatrix.WVP);
+    output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
+    return output;
 }
