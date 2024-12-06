93b96f0a9da11b6933837c8e8c190bf530d2a44b
mutoairi <146906672+mutoairi@users.noreply.github.com>
2024-12-06 12:14:46 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -437,7 +437,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,
-		L"CG2",
+		L"LE2C_25_ムトウ_アイリ",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
