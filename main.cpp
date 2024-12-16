7b99e9023cca3983a863aa0ab391f54dba2a5662
mutoairi <146906672+mutoairi@users.noreply.github.com>
2024-12-16 16:59:07 +0900

    hennkou

--- main.cpp
+++ main.cpp
@@ -94,18 +94,21 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 			position.x *= -1.0f;
 			position.w = 1.0f;
 			positions.push_back(position);
-		}else if(identifier=="vt"){
+		}
+		else if (identifier == "vt") {
 			Vector2 texcoord;
 			s >> texcoord.x >> texcoord.y;
 			texcoord.x = 1.0f - texcoord.x;
 			texcoord.y = 1.0f - texcoord.y;
 			texcoords.push_back(texcoord);
-		}else if(identifier=="vn") {
+		}
+		else if (identifier == "vn") {
 			Vector3 normal;
 			s >> normal.x >> normal.y >> normal.z;
 			normal.x *= -1.0f;
 			normals.push_back(normal);
-		}else if (identifier == "f") {
+		}
+		else if (identifier == "f") {
 
 			VertexData triangle[3];
 			for (uint32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
@@ -151,38 +154,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WinApp* winApp = nullptr;
 	winApp = new WinApp();
 	winApp->Initialize();
-	
 
-;
+
+	;
 
 #pragma endregion
 
 #pragma region"DXCCommonの生成"
 
-//ポインタ
-DirectXCommon* dxcCommon = nullptr;
-//DirectXの初期化
-dxcCommon = new DirectXCommon();
-dxcCommon->Initialize(winApp);
-//HRESULT hr;
-
-
-
-
-
 	//ポインタ
+	DirectXCommon* dxcCommon = nullptr;
+	//DirectXの初期化
+	dxcCommon = new DirectXCommon();
+	dxcCommon->Initialize(winApp);
+	//HRESULT hr;
+
+
+
+
+
+		//ポインタ
 	Input* input = nullptr;
-	
+
 	//入力の初期化
 	input = new Input();
 	input->Initialize(winApp);
 
-	
+
 
 	Logger::Log("HelloDIrectX!\n");
 
 	MSG msg{};
-	
+
 	//SpriteCommon
 	SpriteCommon* spriteCommon = nullptr;
 
@@ -197,8 +200,8 @@ dxcCommon->Initialize(winApp);
 
 	//const uint32_t kSubdivision = 16;
 	ModelData modelData = LoadObjFile("resources", "plane.obj");
-	Microsoft::WRL::ComPtr < ID3D12Resource> vertexResource = dxcCommon->CreateBufferResource( sizeof(VertexData) * modelData.vertices.size());
-	Microsoft::WRL::ComPtr < ID3D12Resource> materialResource = dxcCommon->CreateBufferResource( sizeof(Material));
+	Microsoft::WRL::ComPtr < ID3D12Resource> vertexResource = dxcCommon->CreateBufferResource(sizeof(VertexData) * modelData.vertices.size());
+	Microsoft::WRL::ComPtr < ID3D12Resource> materialResource = dxcCommon->CreateBufferResource(sizeof(Material));
 	//	マテリアルにデータを書き込む
 	Material* materialData = nullptr;
 	//書き込むためのアドレスを取得
@@ -208,7 +211,7 @@ dxcCommon->Initialize(winApp);
 	materialData->enableLighting = true;
 	materialData->uvTransform = MakeIdentity4x4();
 	//wvp用のリソースを作る
-	Microsoft::WRL::ComPtr < ID3D12Resource> wvpResource = dxcCommon->CreateBufferResource( sizeof(TransformationMatrix));
+	Microsoft::WRL::ComPtr < ID3D12Resource> wvpResource = dxcCommon->CreateBufferResource(sizeof(TransformationMatrix));
 	//データを書き込む
 	TransformationMatrix* wvpData = nullptr;
 	//Matrix4x4* transformationMatrixData = nullptr;
@@ -223,7 +226,7 @@ dxcCommon->Initialize(winApp);
 	//リソースの先頭のアドレスから使う
 	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
 	//使用するリソースのサイズは頂点3つ文のサイズ
-	vertexBufferView.SizeInBytes =UINT( sizeof(VertexData)*modelData.vertices.size());
+	vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());
 
 	//1頂点当たりのサイズ
 	vertexBufferView.StrideInBytes = sizeof(VertexData);
@@ -232,9 +235,9 @@ dxcCommon->Initialize(winApp);
 	VertexData* vertexData = nullptr;
 	//書き込むためのアドレスを取得
 	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData)* modelData.vertices.size());
+	std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());
 
-	
+
 	//Spriteを作成
 	std::vector<Sprite*>sprites;
 	for (uint32_t i = 0; i < 5; ++i) {
@@ -245,24 +248,24 @@ dxcCommon->Initialize(winApp);
 		sprites.push_back(sprite);
 	}
 
-	
+
 
 	//平行光源用
-	Microsoft::WRL::ComPtr < ID3D12Resource> directionalLightResource = dxcCommon->CreateBufferResource( sizeof(DirectionalLight));
+	Microsoft::WRL::ComPtr < ID3D12Resource> directionalLightResource = dxcCommon->CreateBufferResource(sizeof(DirectionalLight));
 	DirectionalLight* directionalLightData = nullptr;
 	directionalLightResource->Map(0, nullptr, reinterpret_cast<void**>(&directionalLightData));
 	directionalLightData->color = { 1.0f,1.0f,1.0f,1.0f };
 	directionalLightData->direction = { 0.0f,1.0f,1.0f };
 	directionalLightData->intensity = 1.0f;
 
-	
-	
+
+
 	//2枚目のTextureを読んで転送する
 	TextureManager::GetInstance()->LoadTexture(modelData.material.texturFilePath);
-	
-	
 
-	
+
+
+
 	//SRVを作成するDescriptorHeapの場所を決める
 	/*D3D12_CPU_DESCRIPTOR_HANDLE textureSrvHandleCPU = dxcCommon->GetSRVCPUDescriptorHandle(1);
 	D3D12_GPU_DESCRIPTOR_HANDLE textureSrvHandleGPU = dxcCommon->GetSRVGPUDescriptorHandle(1);*/
@@ -282,13 +285,13 @@ dxcCommon->Initialize(winApp);
 
 	Transform transform{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
 	Transform cameraTransform{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f} };
-	
-	
+
+
 	bool useMonsterBall = true;
 	//ウィンドウのボタンが押されるまでループ
 	while (msg.message != WM_QUIT) {
 
-		
+
 
 		//windowにメッセージが来てたら最優先で処理させる
 		if (winApp->ProcessMessage()) {
@@ -353,7 +356,7 @@ dxcCommon->Initialize(winApp);
 				//Spriteの更新
 				sprite->Update();
 			}
-			
+
 			ImGui::End();
 
 			//ImGUi内部のコマンドを生成する
@@ -364,7 +367,7 @@ dxcCommon->Initialize(winApp);
 
 			//Spriteの描画準備
 			spriteCommon->CommonDrawing();
-			
+
 			//dxcCommon->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);//VBVを設定
 			////マテリアルCBufferの場所を設定
 			//dxcCommon->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
@@ -380,13 +383,13 @@ dxcCommon->Initialize(winApp);
 
 			//
 
-			
+
 			//平行光源
 			dxcCommon->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource->GetGPUVirtualAddress());
 
 			//Sprite描画！(DrawCall/ドローコール)。3頂点出一つのインスタンス。インスタンスについては今後
 			for (Sprite* sprite : sprites) {
-				
+
 				sprite->Draw();
 			}
 			//
@@ -396,7 +399,7 @@ dxcCommon->Initialize(winApp);
 
 
 
-			
+
 
 			//ゲーム処理
 
@@ -406,10 +409,10 @@ dxcCommon->Initialize(winApp);
 	ImGui_ImplWin32_Shutdown();
 	ImGui::DestroyContext();
 	OutputDebugStringA("HelloDirectX!\n");
-	
+
 	//解放処理
 	//CloseHandle(fenceEvent);
-	
+
 	TextureManager::GetInstance()->Finalize();
 	//入力解放
 	delete input;
@@ -420,7 +423,7 @@ dxcCommon->Initialize(winApp);
 	}
 	//CloseWindow
 	winApp->Finalize();
-	
+
 	delete winApp;
 
 
