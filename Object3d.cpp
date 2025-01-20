de5b11a24a3b0084420f72ebac3db81eb94dde5f
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-01-20 17:08:46 +0900

    8割完成

--- Object3d.cpp
+++ Object3d.cpp
@@ -1,5 +1,162 @@
 #include "Object3d.h"
+#include"Object3dCommon.h"
+#include"TextureManager.h"
 
-void Object3d::Initialize()
+void Object3d::Initialize(Object3dCommon* object3dCommon)
 {
+	this->object3dCommon = object3dCommon;
+	modelData = LoadObjFile("resources", "plane.obj");
+	Vertex();
+	MaterialInitialize();
+	TransformInitialize();
+	EnableLightInitialize();
+	//.objの参照しているテクスチャの読み込み
+	TextureManager::GetInstance()->LoadTexture(modelData.material.texturFilePath);
+	modelData.material.textureIndex = TextureManager::GetInstance()->GetTextureIndexByFilePath(modelData.material.texturFilePath);
+}
+
+MaterialData Object3d::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
+{
+	MaterialData materialData;
+	std::string line;
+
+	std::ifstream file(directoryPath + "/" + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		if (identifier == "map_Kd") {
+			std::string textureFilename;
+			s >> textureFilename;
+
+			materialData.texturFilePath = directoryPath + "/" + textureFilename;
+		}
+	}
+	return materialData;
+}
+
+ModelData Object3d::LoadObjFile(const std::string& directoryPath, const std::string& filename)
+{
+	ModelData modelData;
+	std::vector<Vector4> positions;
+	std::vector<Vector3> normals;
+	std::vector<Vector2> texcoords;
+	std::string line;
+
+	std::ifstream file(directoryPath + "/" + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+		if (identifier == "v") {
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.x *= -1.0f;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt") {
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoord.x = 1.0f - texcoord.x;
+			texcoord.y = 1.0f - texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn") {
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normal.x *= -1.0f;
+			normals.push_back(normal);
+		}
+		else if (identifier == "f") {
+
+			VertexData triangle[3];
+			for (uint32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element) {
+					std::string index;
+					std::getline(v, index, '/');
+					elementIndices[element] = std::stoi(index);
+
+				}
+				Vector4 position = positions[elementIndices[0] - 1];
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				Vector3 normal = normals[elementIndices[2] - 1];
+				VertexData vertex = { position,texcoord,normal };
+				modelData.vertices.push_back(vertex);
+				triangle[faceVertex] = { position,texcoord,normal };
+			}
+			modelData.vertices.push_back(triangle[2]);
+			modelData.vertices.push_back(triangle[1]);
+			modelData.vertices.push_back(triangle[0]);
+		}
+		else if (identifier == "mtllib") {
+			std::string materialFilename;
+			s >> materialFilename;
+			modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
+		}
+	}
+	return modelData;
+}
+
+void Object3d::Vertex()
+{
+	//====================    頂点    ===========================
+
+	vertexResource = object3dCommon->GetDxcCommon()->CreateBufferResource(sizeof(VertexData) * modelData.vertices.size());
+
+	//リソースの先頭のアドレスから使う
+	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	//使用するリソースのサイズは頂点3つ文のサイズ
+	vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());
+
+	//1頂点当たりのサイズ
+	vertexBufferView.StrideInBytes = sizeof(VertexData);
+	//書き込むためのアドレスを取得
+	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());
+}
+
+void Object3d::MaterialInitialize()
+{
+	//===================　マテリアル　==========================
+
+	materialResource = object3dCommon->GetDxcCommon()->CreateBufferResource(sizeof(Material));
+	//書き込むためのアドレスを取得
+	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
+	//今回は赤を書き込んでみる
+	materialData->color = { Vector4(1.0f, 1.0f, 1.0f, 1.0f) };
+	materialData->enableLighting = true;
+	materialData->uvTransform = MakeIdentity4x4();
+
+}
+
+void Object3d::TransformInitialize()
+{
+	wvpResource = object3dCommon->GetDxcCommon()->CreateBufferResource(sizeof(TransformationMatrix));
+	//書き込むためのアドレスを取得
+	wvpResource->Map(0, nullptr, reinterpret_cast<void**>(&wvpData));
+	//単位行列を書き込んでおく
+	wvpData->WVP = MakeIdentity4x4();
+	wvpData->World = MakeIdentity4x4();
+}
+
+void Object3d::EnableLightInitialize()
+{
+	//平行光源用
+	Microsoft::WRL::ComPtr < ID3D12Resource> directionalLightResource = object3dCommon->GetDxcCommon()->CreateBufferResource(sizeof(DirectionalLight));
+	DirectionalLight* directionalLightData = nullptr;
+	directionalLightResource->Map(0, nullptr, reinterpret_cast<void**>(&directionalLightData));
+	directionalLightData->color = { 1.0f,1.0f,1.0f,1.0f };
+	directionalLightData->direction = { 0.0f,1.0f,1.0f };
+	directionalLightData->intensity = 1.0f;
 }
--- Object3d.h
+++ Object3d.h
@@ -1,7 +1,64 @@
 #pragma once
+#include"makeMatrix.h"
+#include<Windows.h>
+#include<string>
+#include<vector>
+#include<wrl.h>
+#include<cstdint>
+
+struct Material
+{
+	Vector4 color;
+	int32_t enableLighting;
+	float padding[3];
+	Matrix4x4 uvTransform;
+};
+
+struct MaterialData
+{
+	std::string texturFilePath;
+	uint32_t textureIndex=0;
+};
+struct ModelData
+{
+	std::vector<VertexData> vertices;
+	MaterialData material;
+};
+
+class Object3dCommon;
 class Object3d
 {
 public:
-	void Initialize();
+	void Initialize(Object3dCommon*object3dCommon);
+
+	//.mtlファイルの読み取り
+	struct MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
+	//.objファイルの読み取り
+	struct ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename);
+private:
+	void Vertex();
+	void MaterialInitialize();
+	void TransformInitialize();
+	void EnableLightInitialize();
+	Object3dCommon* object3dCommon=nullptr;
+	//OBJファイルデータ
+	ModelData modelData;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource>vertexResource;
+	Microsoft::WRL::ComPtr < ID3D12Resource> materialResource;
+	//wvp用のリソースを作る
+	Microsoft::WRL::ComPtr < ID3D12Resource> wvpResource;
+	Microsoft::WRL::ComPtr < ID3D12Resource> directionalLightResource;
+
+	//頂点バッファビューを作成する
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	//頂点リソースにデータを書き込む
+	VertexData* vertexData = nullptr;
+	//	マテリアルにデータを書き込む
+	Material* materialData = nullptr;
+	//データを書き込む
+	TransformationMatrix* wvpData = nullptr;
+	//平行光源
+	DirectionalLight* directionalLightData = nullptr;
 };
 
