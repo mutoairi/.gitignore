42b50631f3521cb2ae988a3808b9a528adad638a
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-02-12 12:26:23 +0900

    jbj

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -8,8 +8,7 @@
 #include"externals/imugui/imgui_impl_win32.h"
 #include"externals/DirectXTex/d3dx12.h"
 
-#pragma comment(lib,"d3d12.lib"
-)
+#pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"dxcompiler.lib")
 
